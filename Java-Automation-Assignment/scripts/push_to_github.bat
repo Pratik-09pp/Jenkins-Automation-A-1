077d3a9c4c1fafba5bc056acb8162026e0b51030
Pratik Patil <110323843+Pratik-09pp@users.noreply.github.com>
2025-04-29 12:28:58 +0530

    Update push_to_github.bat

--- Java-Automation-Assignment/scripts/push_to_github.bat
+++ Java-Automation-Assignment/scripts/push_to_github.bat
@@ -2,44 +2,47 @@
 echo -----------------------------------------------------
 echo Pushing archive file to GitHub...
 echo -----------------------------------------------------
-cd
-REM Move to Java-Automation-Assignment folder
+
+REM Move up one directory (into Java-Automation-Assignment folder)
 cd ..
 
-cd
-
-REM Now you are inside Java-Automation-Assignment (No need for another cd)
-
 REM Set important paths
-set ARCHIVE_DIR=archives
-set DEST_DIR=..\Build-Archieves
+set "ARCHIVE_DIR=archives"
+set "DEST_DIR=..\Build-Archieves"
 
-REM Read the PAT passed from Jenkins
-set GITHUB_PAT=%1
+REM Read the GitHub Personal Access Token passed from Jenkins
+set "GITHUB_PAT=%1"
 
 REM Set GitHub URL using username and PAT
-set GITHUB_URL=https://Pratik-09pp:%GITHUB_PAT%@github.com/Pratik-09pp/Jenkins-Automation-A-1.git
+set "GITHUB_URL=https://Pratik-09pp:%GITHUB_PAT%@github.com/Pratik-09pp/Jenkins-Automation-A-1.git"
 
-cd
+REM Check if this is a Git repository
+if not exist ".git" (
+    echo Not a Git repository! Exiting...
+    exit /b 1
+)
 
-
-REM Find the zip file (assuming only one zip file)
-for %%f in (%ARCHIVE_DIR%\*.zip) do set ZIP_FILE=%%f
+REM Find the zip file inside archives folder (assuming only one zip file)
+for %%f in ("%ARCHIVE_DIR%\*.zip") do (
+    set "ZIP_FILE=%%f"
+    set "ZIP_FILENAME=%%~nxf"
+)
 
 REM Copy the zip file to Build-Archieves folder
+echo Copying %ZIP_FILE% to %DEST_DIR%...
 copy "%ZIP_FILE%" "%DEST_DIR%"
 
 REM Stage the copied file for commit
-git add "../Build-Archieves/%~nxZIP_FILE%"
+echo Staging file for Git commit...
+git add "%DEST_DIR%\%ZIP_FILENAME%"
 
 REM Commit the change
-git commit -m "Add new build archive: %~nxZIP_FILE%"
+echo Creating Git commit...
+git commit -m "Add new build archive: %ZIP_FILENAME%"
 
 REM Push to GitHub
-git push %GITHUB_URL% main
-
-
-cd
+echo Pushing to GitHub...
+git push "%GITHUB_URL%" main
 
 IF %ERRORLEVEL% EQU 0 (
     echo Archive pushed successfully to GitHub!
